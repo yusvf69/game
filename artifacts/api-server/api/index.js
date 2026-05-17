6dfafc1c937dbdf3582506a21934c5831cf08382
youssef mohamed <youssefmohamed@youssefs-MacBook-Pro.local>
2026-05-18 02:33:01 +0300

    deploy: cleanup workaround dirs, trigger frontend

--- artifacts/api-server/api/index.js
+++
@@ -1,9 +0,0 @@
-let cachedApp;
-
-export default async function handler(req, res) {
-  if (!cachedApp) {
-    const { default: app } = await import("../src/app.js");
-    cachedApp = app;
-  }
-  return cachedApp(req, res);
-}
